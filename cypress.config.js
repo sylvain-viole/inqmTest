cd67ff995f0b04b8af899f3afadfd8baa677c9d8
Sylvain Viole <sylvain.viole@yousign.com>
2022-10-08 08:40:55 +0200

    [CHORE] Add Cypress Chrome launchOptions + add npm scripts

--- cypress.config.js
+++ cypress.config.js
@@ -27,6 +27,28 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'chrome') {
+          launchOptions.preferences.default.intl = { accept_languages: 'fr' };
+          // https://github.com/GoogleChrome/chrome-launcher/blob/master/docs/chrome-flags-for-tools.md
+          launchOptions.args.push('--disable-dev-shm-usage');
+          launchOptions.args.push('--js-flags=--expose-gc');
+          launchOptions.args.push('--num-raster-threads=4');
+          launchOptions.args.push('--disable-background-networking');
+          launchOptions.args.push('--disable-default-apps');
+          launchOptions.args.push('--disable-gpu');
+          launchOptions.args.push('--disable-sync');
+          launchOptions.args.push('--disable-features=Translate');
+          launchOptions.args.push('--metrics-recording-only');
+          launchOptions.args.push('--mute-audio');
+          launchOptions.args.push('--no-first-run');
+          launchOptions.args.push('--disable-client-side-phishing-detection');
+          launchOptions.args.push('--no-default-browser-check');
+          launchOptions.args.push('--auto-open-devtools-for-tabs');
+
+          return launchOptions;
+        }
+      });
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'https://www.welcometothejungle.com/',
